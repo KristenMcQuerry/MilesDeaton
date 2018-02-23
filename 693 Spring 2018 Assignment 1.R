fccaf3cbdedb405dc03e051b6eac8b0c38b7f3a1
Deaton <mde266@uky.edu>
2018-02-23 17:38:05 -0500

    Filters and sorting
    Completed part 3 of assignment 1.

--- 693 Spring 2018 Assignment 1.R
+++ 693 Spring 2018 Assignment 1.R
@@ -8,3 +8,50 @@ library(nycflights13)
 
 ## dataset for assignment
 flights
+
+#3a
+flights %>% 
+  filter(arr_delay>2*60)
+
+
+#3b 
+flights %>% 
+  filter(dest=="IAH"|| "HOU" )
+
+#3c
+flights %>% 
+  filter(carrier %in% c("DL","AA","UA"))
+
+#3d
+flights %>% 
+  filter(month==6,7,8)
+         
+#3e 
+flights %>% 
+  filter(arr_delay>2*60 & dep_delay<1)
+
+#3f
+flights %>% 
+  filter(arr_delay<-30 & dep_delay>=60)
+
+#3g
+flights %>% 
+  filter(dep_time<=600)
+
+#3h
+flights %>% 
+  arrange(desc(dept_delay)) #most delayed
+flights %>% 
+  arrange(dept_delay) #left the earliest
+
+
+#3i
+flights %>% 
+  arrange(air_time) 
+
+#3j
+flights %>% 
+  arrange(desc(distance)) #longest
+flights %>% 
+  arrange(distance) #shortest
+
